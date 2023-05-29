c103fc516795b87301630e9c05867b9d3fef37d3
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-05-29 16:29:55 +0200

    Version 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kraken.MixProject do
   def project do
     [
       app: :kraken,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
