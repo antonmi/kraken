937b3d08278dcc91ff98bc3af0c03406387edc10
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-09-13 12:01:50 +0200

    Set version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kraken.MixProject do
   def project do
     [
       app: :kraken,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
